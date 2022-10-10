3117e093a80e30679812d46cdc2dbd6a3ab1dc59
diogo.fontes <diogo.fontes@ua.pt>
2022-10-10 16:25:59 +0100

    ex2

--- Lab1/sndfile-example-src/wav_hist.h
+++ Lab1/sndfile-example-src/wav_hist.h
@@ -5,10 +5,13 @@
 #include <vector>
 #include <map>
 #include <sndfile.hh>
+#include <fstream>
 
 class WAVHist {
   private:
 	std::vector<std::map<short, size_t>> counts;
+	std::map<short,size_t> mid_channel;
+	std::map<short, size_t> side_channel;
 
   public:
 	WAVHist(const SndfileHandle& sfh) {
@@ -19,7 +22,31 @@ class WAVHist {
 		size_t n { };
 		for(auto s : samples)// reconhece o tipo automaticamente
 			counts[n++ % counts.size()][s]++;
+	
+
+	for(n = 0; n+1 < samples.size(); n += 2)
+	{
+		mid_channel[((samples[n] + samples[n+1])/2)]++;
+		side_channel[((samples[n] - samples[n+1])/2)]++;
+
 	}
+
+	 //MID CHANNEL
+		std::ofstream out_file("MID_channel.txt");
+		for(auto [value, counter] : mid_channel)
+			out_file << value << '\t' << counter << '\n';
+		out_file.close();
+
+		//SIDE CHANNEL
+		std::ofstream out_file2("SIDE_channel.txt");
+		for(auto [value, counter] : side_channel)
+			out_file2 << value << '\t' << counter << '\n';
+
+	}
+
+
+
+
 
 	void dump(const size_t channel) const { // O const 
 		for(auto [value, counter] : counts[channel])
