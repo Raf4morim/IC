8395a2d5cb744c3014c5e542b0e1d7ad38a0fe59
Rafael Amorim <rafael.amorim@ua.pt>
2022-10-17 16:39:58 +0100

    decoder.cpp

---
+++ Lab1/sndfile-example-src/decoder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bit_stream.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    if (argc < 2) {
+        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
+        return 1;
+    }
+    BitStream bs(argv[argc - 1]);
+    cout << bs.readBits(3) << endl;  
+}
