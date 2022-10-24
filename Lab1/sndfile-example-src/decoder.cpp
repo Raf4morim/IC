e6d95dd8138c808f2582f1cc072752b1255f92db
Rafael Amorim <rafael.amorim@ua.pt>
2022-10-24 17:07:57 +0100

    dec

--- Lab1/sndfile-example-src/decoder.cpp
+++ Lab1/sndfile-example-src/decoder.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
-#include <bit_stream.h>
+#include "bit_stream.h"
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main (int argc, char *argv[]) {	
 
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <input_file>" << endl;
-        return 1;
+    //this program converts a binary file and converts it to a ".txt" file that contains only 1's and 0's using the bit_stream class
+
+    if (argc < 3) {	
+        cerr << "Usage: " << argv [0] << " <input file> <output file>\n" ;
+        return 1 ;
     }
-    BitStream bs(argv[argc - 1]);
-    cout << bs.readBits(3) << endl;  
+
+    //open the input file
+    BitStream inputFile (argv [1], "r") ;
+    
+    //open the output file
+    ofstream outputFile (argv [2], ios::out) ;
+    if (! outputFile) {
+        cerr << "Error: could not open output file " << argv [2] << ".\n" ;
+        return 1 ;
+    }
+
+    //read the bits from the input file
+    vector<int> bits;
+    bits = inputFile.readBits(inputFile.getFileSize() * 8);
+    inputFile.close();
+
+    //write the bits to the output file
+    for (int i = 0; i < bits.size(); i++){
+        outputFile << bits[i];
+    }
+    outputFile.close();
+
+    return 0;
 }
