f3c841150b4b0c5c2bb60590b18881994f1cba78
Rafael Amorim <rafael.amorim@ua.pt>
2022-10-25 18:02:10 +0100

    sjkdakjd

--- Lab1/sndfile-example-src/encoder.cpp
+++ Lab1/sndfile-example-src/encoder.cpp
@@ -4,26 +4,23 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {	
-
-    //this program converts a ".txt" file that contains only 1's and 0's into a binary file using the bit_stream class
+    ifstream iFile (argv [1], ios::in) ;                                    
+    string oFile = argv[2];
 
     if (argc != 3) {	
         cerr << "Usage: " << argv [0] << " <input file> <output file>\n" ;
         return 1 ;
     } 
 
-    ifstream iFile (argv [1], ios::in) ;
     if (! iFile) {
         cerr << "Error: could not open input file.\n" ;
         return 1 ;
     }
 
-    string oFile = argv[2];
-
     string line;
-    getline(iFile, line);
-    cout << "Input file: " << line << endl;
-    cout << "Input file length: " << line.length() << endl;
+    getline(iFile, line);                                       
+    cout << "Input file: " << line << endl;                     
+    cout << "Input file length: " << line.length() << endl;  
     iFile.close();
 
     BitStream outputFile (oFile, "w") ;
